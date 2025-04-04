dbde0001f0a94db2836032ece54de1cc9684bf6e
p1n9d3v <p1n9m4i1@gmail.com>
2025-04-04 21:56:55 +0900

    feat: react-native-paper 라이브러리 추가 및 헤더 UI 업데이트
    - react-native-paper 라이브러리 추가하여 헤더 UI 구현
    - SafeAreaProvider 추가하여 화면 안전 영역 처리
    - 헤더에 메뉴, 캘린더, 로그인 버튼 추가

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {
     StaticParamList,
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DayScreen } from './src/components/screens/day';
 import { HomeScreen } from './src/components/screens/home';
 
@@ -12,12 +13,19 @@ const RootStack = createNativeStackNavigator({
         Home: HomeScreen,
         Day: DayScreen,
     },
+    screenOptions: {
+        headerShown: false,
+    },
 });
 
 const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-    return <Navigation />;
+    return (
+        <SafeAreaProvider>
+            <Navigation />
+        </SafeAreaProvider>
+    );
 }
 
 type RootStackParamList = StaticParamList<typeof RootStack>;
--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,22 +1,35 @@
-import { MaterialIcons } from 'expo-vector-icons';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import { colors, theme } from '../constants/colors';
 
 export const Header = () => {
+    const handleLogin = () => {
+        console.log('login');
+    };
     return (
-        <Appbar.Header style={styles.header}>
-            <View>
-                <View>
-                    <TouchableOpacity>
-                        <MaterialIcons
-                            name="menu"
-                            size={24}
-                            color={colors.gray[400]}
-                        />
-                    </TouchableOpacity>
-                </View>
-            </View>
+        <Appbar.Header style={styles.header} mode="center-aligned">
+            <Appbar.Action
+                icon="menu"
+                onPress={handleLogin}
+                iconColor={colors.gray[400]}
+                size={20}
+            />
+            <Appbar.Content
+                title="Home"
+                titleStyle={{ color: colors.gray[400] }}
+            />
+            <Appbar.Action
+                icon="calendar"
+                onPress={handleLogin}
+                iconColor={colors.gray[400]}
+                size={20}
+            />
+            <Appbar.Action
+                icon="login"
+                onPress={handleLogin}
+                iconColor={colors.gray[400]}
+                size={20}
+            />
         </Appbar.Header>
     );
 };
@@ -26,5 +39,10 @@ const styles = StyleSheet.create({
         backgroundColor: theme.background,
         borderBottomWidth: 1,
         borderBottomColor: theme.border,
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    loginButton: {
+        color: theme.text,
     },
 });
